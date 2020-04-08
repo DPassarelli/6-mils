dac24fcfc226c1a7eb780c4b360799d6c85710a8
David Passarelli <dpassarelli@camelotcg.com>
2020-04-08 10:39:46 -0400

    Working on separating functional tests from unit.

--- test/functional/full-punchout-setup.test.js
+++ test/functional/full-punchout-setup.test.js
@@ -1,6 +1,8 @@
 /* eslint-env mocha */
 
-const nock = require('nock')
+const debug = require('debug')('6-mils:func-test')
+const fs = require('fs')
+const http = require('http')
 const path = require('path')
 
 /**
@@ -9,73 +11,88 @@ const path = require('path')
  */
 const cxml = require('../../main.js')
 
+let server = null
+let ASSIGNED_PORT = 0
+
 /**
- * A list of all requests captured by `nock`. This will allow the test to
- * include assertions against the request in addition to the response.
+ * A list of all requests captured by the test listener.
  * @type {Array}
  */
 const requests = []
 
-/**
- * Which outgoing HTTP requests that will be intercepted. This is needed to
- * hook into the `request` event, which allows the intercepted request to be
- * captured for further analysis.
- * @type {Object}
- */
-let scope = null
+before(function () {
+  server = http.createServer((req, res) => {
+    switch (req.url) {
+      case '/posr/success':
+        res.setHeader('content-type', 'application/xml')
+        fs.createReadStream(path.join(__dirname, '../samples/PunchOutSetupResponse-200.xml')).pipe(res)
+        res.end()
+        break
+
+      default:
+        res.end(/\d+/.exec(req.url)[0])
+    }
+  })
+  server.listen(0, '127.0.0.1', () => { ASSIGNED_PORT = server.address().port; debug('LOCAL TEST SERVER LISTENING ON', JSON.stringify(server.address())) })
+})
+
+after(function (done) {
+  server.on('close', () => { console.log('LOCAL TEST SERVER CLOSED'); done() })
+  server.close()
+})
 
 describe('the PunchOut Request/Response cycle', function () {
-  before(function () {
-    scope = nock('http://cxml.org')
-      .post('/posr/success')
-      .replyWithFile(
-        200,
-        path.join(__dirname, '../samples/PunchOutSetupResponse-200.xml'),
-        {
-          'content-type': 'application/xml'
-        }
-      )
-      .persist()
+  context('with a successful response', function () {
+    it('must be fulfilled', function () {
+      const posreq = new cxml.PunchOutSetupRequest()
 
-    scope.on('request', (request, interceptor, body) => {
-      requests.push(body)
+      return posreq
+        .setBuyerInfo({ domain: 'DUNS', id: '987654' })
+        .setSupplierInfo({ domain: 'DUNS', id: '123456' })
+        .setSenderInfo({ domain: 'NetworkId', id: 'example.com', secret: 'Open sesame!' })
+        .submit(`http://localhost:${ASSIGNED_PORT}/posr/success`)
+        .then(function (response) {
+          debug('%j', response)
+          /**
+           * Verify that the response contains the correct status code and text.
+           */
+          expect(response.statusCode).to.equal('200')
+          expect(response.statusText).to.equal('success')
+
+          /**
+           * Verify that the response contains the correct URL to redirect to.
+           */
+          expect(response.url).to.equal('http://xml.workchairs.com/retrieve?reqUrl=20626;Initial=TRUE')
+
+          /**
+           * Verify that the original request has the correct timestamp.
+           */
+          const requestBody = requests.pop()
+          const timestamp = new Date(/timestamp="[^"]+/.exec(requestBody)[0].substring(11))
+          const now = new Date()
+          const diffInMilliseconds = now.getTime() - timestamp.getTime()
+
+          expect(diffInMilliseconds).to.be.lessThan(1000)
+        })
     })
   })
 
-  after(function () {
-    nock.cleanAll()
-    nock.restore()
-  })
+  context('with a failed HTTP connection', function () {
+    it('must be rejected', function (done) {
+      const posreq = new cxml.PunchOutSetupRequest()
 
-  it('must work when a successful response is received', function () {
-    const posreq = new cxml.PunchOutSetupRequest()
-
-    return posreq
-      .setBuyerInfo({ domain: 'DUNS', id: '987654' })
-      .setSupplierInfo({ domain: 'DUNS', id: '123456' })
-      .setSenderInfo({ domain: 'NetworkId', id: 'example.com', secret: 'Open sesame!' })
-      .submit('http://cxml.org/posr/success')
-      .then(function (response) {
-        /**
-         * Verify that the response contains the correct status code and text.
-         */
-        expect(response.statusCode).to.equal('200')
-        expect(response.statusText).to.equal('success')
-
-        /**
-         * Verify that the original request has the correct timestamp.
-         */
-        const requestBody = requests.pop()
-        const timestamp = new Date(/timestamp="[^"]+/.exec(requestBody)[0].substring(11))
-        const now = new Date()
-        const diffInMilliseconds = now.getTime() - timestamp.getTime()
-
-        expect(diffInMilliseconds).to.be.lessThan(1000)
-
-        /**
-         * Verify that the response contains the correct URL to redirect to.
-         */
-        expect(response.url).to.equal('http://xml.workchairs.com/retrieve?reqUrl=20626;Initial=TRUE')
-      })
+      posreq
+        .setBuyerInfo({ domain: 'DUNS', id: '987654' })
+        .setSupplierInfo({ domain: 'DUNS', id: '123456' })
+        .setSenderInfo({ domain: 'NetworkId', id: 'example.com', secret: 'Open sesame!' })
+        .submit(`http://localhost:${ASSIGNED_PORT + 1}/posr/timeout`)
+        .then(function (response) {
+          done(new Error('The promise was not rejected'))
+        })
+        .catch(function (err) {
+          console.log(err)
+          done()
+        })
+    })
   })
 })
